d8593d34ef188f604b7aef2e2b1676505740422a
crumbyte <oleksiivodotyiets@gmail.com>
2025-05-25 18:12:07 +0300

    Add CLI flag allowing to exclude dirs from scanning.

--- structure/tree.go
+++ structure/tree.go
@@ -119,6 +119,8 @@ func (t *Tree) Traverse() error {
 		currentNode *Entry
 	)
 
+	drive.InoFilterInstance.Reset()
+
 	if t.root == nil || !t.root.IsDir {
 		return nil
 	}
@@ -127,7 +129,7 @@ func (t *Tree) Traverse() error {
 
 	for len(queue) > 0 {
 		currentNode, queue = queue[0], queue[1:]
-
+▄
 		t.handleEntry(
 			currentNode,
 			func(newDir *Entry) { queue = append(queue, newDir) },
