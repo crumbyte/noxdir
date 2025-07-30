47d38070cf8a56514f1465d7e2df5f9a35566673
crumbyte <oleksiivodotyiets@gmail.com>
2025-07-30 22:37:13 +0300

    Fix the inconsistent command execution flow.
    Signed-off-by: crumbyte <oleksiivodotyiets@gmail.com>

--- command/command.go
+++ command/command.go
@@ -10,39 +10,30 @@ import (
 	"github.com/spf13/pflag"
 )
 
-// RootCmd represents a root command for all subcommands. This command does not
-// contain any behavior but only proxies the execution to the subcommands.
-var RootCmd = &cobra.Command{
-	SilenceUsage:       true,
-	DisableSuggestions: true,
-}
+func NewRootCmd() *cobra.Command {
+	rootCmd := &cobra.Command{}
 
-func init() {
-	RootCmd.AddCommand(archive.PackCmd, archive.UnpackCmd)
+	rootCmd.AddCommand(archive.NewPackCmd(), archive.NewUnpackCmd())
 
-	archive.PackCmd.SetHelpFunc(
-		func(cmd *cobra.Command, _ []string) {
+	for _, cmd := range rootCmd.Commands() {
+		cmd.SetHelpFunc(func(cmd *cobra.Command, _ []string) {
 			_, _ = io.WriteString(cmd.OutOrStdout(), "Usage: "+ViewHelp(cmd))
-		},
-	)
+		})
+	}
 
-	archive.UnpackCmd.SetHelpFunc(
-		func(cmd *cobra.Command, _ []string) {
-			_, _ = io.WriteString(cmd.OutOrStdout(), "Usage: "+ViewHelp(cmd))
-		},
-	)
+	return rootCmd
 }
 
 // Execute executes the root command. It requires the input and output sources,
 // as otherwise it'll try to read the STDIN and STDOUT streams. The "args" value
 // contains a complete command expression split into a slice of strings, and the
 // "out" represents io.Writer instance for writing the command execution result.
-func Execute(args []string, out io.Writer) error {
-	RootCmd.SetOut(out)
-	RootCmd.SetErr(out)
-	RootCmd.SetArgs(args)
+func Execute(root *cobra.Command, args []string, out io.Writer) error {
+	root.SetOut(out)
+	root.SetErr(out)
+	root.SetArgs(args)
 
-	return RootCmd.Execute()
+	return root.Execute()
 }
 
 func ViewHelp(cmd *cobra.Command) string {
