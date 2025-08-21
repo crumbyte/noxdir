3757ca055750fe4d5b56a3522f27b7fcd48fbeb3
Rui Jiang <me@ruijiang.me>
2025-08-21 08:45:08 -0500

    Added nix flake (#85)

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "High-performance, cross-platform command-line tool for visualizing and exploring your file system usage";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            golangci-lint
+          ];
+        };
+
+        packages.noxdir = pkgs.buildGoModule {
+          pname = "noxdir";
+          version = "0.7.0";
+          src = ./.;
+          vendorHash = "sha256-NtrTLF6J+4n+bnVsfs+WAmlYeL0ElJzwaiK1sk59z9k=";
+          ldflags = [
+            "-s"
+            "-w"
+          ];
+          subPackages = [ "." ];
+        };
+
+        packages.default = self.packages.${system}.noxdir;
+      }
+    );
+}
