c3ed533e523f39a373dbf0e4da61cb96d183ee8f
crumbyte <oleksiivodotyiets@gmail.com>
2025-10-02 00:58:22 +0300

    Use native fstatat implementation for arm arch.
    Signed-off-by: crumbyte <oleksiivodotyiets@gmail.com>

--- drive/linux.go
+++ drive/linux.go
@@ -10,17 +10,13 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
-const (
-	NTFSSbMagic   = 0x5346544e
-	SysNewFstatat = 262
-)
+const NTFSSbMagic = 0x5346544e
 
 const mountInfoPath = "/proc/self/mounts"
 
@@ -260,46 +256,6 @@ func Explore(path string) error {
 	}()
 
 	return nil
-}
-
-func fstatat(alloc Allocator, dirFD int, path string, stat *unix.Stat_t, flags int) (err error) {
-	var _p0 *byte
-
-	_p0, err = bytePtrFromString(alloc, path)
-	if err != nil {
-		return
-	}
-
-	_, _, e1 := syscall.Syscall6(
-		SysNewFstatat,
-		uintptr(dirFD),
-		uintptr(unsafe.Pointer(_p0)),
-		uintptr(unsafe.Pointer(stat)),
-		uintptr(flags),
-		0,
-		0,
-	)
-	if e1 != 0 {
-		return e1
-	}
-
-	return
-}
-
-func bytePtrFromString(alloc Allocator, s string) (*byte, error) {
-	if strings.IndexByte(s, 0) != -1 {
-		return nil, syscall.EINVAL
-	}
-
-	//nolint:gosec
-	buf, err := alloc.Alloc(uint32(len(s) + 1))
-	if err != nil {
-		return nil, err
-	}
-
-	copy(buf, s)
-
-	return &buf[0], nil
 }
 
 func bytePtrToString(alloc Allocator, bytes []byte) string {
