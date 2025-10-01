711ba8a45b148979e2a5080e43799d5371d1ce6a
crumbyte <oleksiivodotyiets@gmail.com>
2025-10-02 01:02:47 +0300

    Use native fstatat implementation for arm arch.
    Signed-off-by: crumbyte <oleksiivodotyiets@gmail.com>

--- drive/linux_fstatat_arm64.go
+++ drive/linux_fstatat_arm64.go
@@ -4,6 +4,6 @@ package drive
 
 import "golang.org/x/sys/unix"
 
-func fstatat(fd int, name string, stat *unix.Stat_t, flags int) error {
+func fstatat(_ Allocator, fd int, name string, stat *unix.Stat_t, flags int) error {
 	return unix.Fstatat(fd, name, stat, flags)
 }
