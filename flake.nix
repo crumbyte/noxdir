acffa7f262365ad95588d566e541c306116048ba
Rui Jiang <me@ruijiang.me>
2025-10-13 07:57:26 -0500

    0.9.0 Nix flake update

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
         packages.noxdir = pkgs.buildGoModule {
           pname = "noxdir";
-          version = "0.8.0";
+          version = "0.9.0";
           src = ./.;
           vendorHash = "sha256-uRJP21bJ8NlJ0qOG81Gax9LJ+HdPfxLKj1Jjzbweync=";
           ldflags = [
